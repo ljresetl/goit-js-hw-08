75336981ec6c852368b6d3d567e303c0c6bb7551
jres <ljresetl@gmail.com>
2025-05-21 20:36:52 +0200

    up

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", () => {
-  const trimmedValue = inputEl.value.trim();
-  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
